ee845a57f160c1b1658d20525f8b0da8c2cb562f
WHomer <billyhomeriii@gmail.com>
2017-05-19 22:59:48 -0500

    master adding updated content css, html, JS

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -2,42 +2,42 @@
 
 //set word variable
 var word = 'hello';
-var guessedCharacter = 'a';
+var guessedCharacter = 'ab';
 var guessesLeft = 10;
 
 //listen for on keyup
 document.addEventListener('keyup', function(e){
-	//store user input
-	var userGuessedChar = e.key;
-	
-	//convert to lowercase
-	userGuessedChar = userGuessedChar.toLowerCase();
+	//store user input and convert to lowercase
+	var userGuessedChar = e.key.toLowerCase();
+	console.log(userGuessedChar);
 	
 	//check to see if the guessed letter is a-z
 	if (isLetter(userGuessedChar)){
 		//check if letter was already guessed
-		if (userGuessedChar.search(guessedCharacter) === -1) {
+		if (guessedCharacter.search(userGuessedChar) > -1) {
 			//character already guessed
 			console.log('character already guessed');
 		} else {
 			//if new character
 			//check if word contains character
-			if (userGuessedChar.search(word) > -1) {
+			if (word.search(userGuessedChar) > -1) {
 				//highlight letter
-				console.log('one step closer');
-				//add to already guessed
-				guessedCharacter = guessedCharacter + userGuessedChar;
-			}
+				console.log('one step closer ');
 				
-			//if word does not contain character
-				//increase incorrectCount
-				//add to already guessed
+			} else {
+				//if word does not contain character
+				//decrease guessesLeft by 1
+				guessesLeft--;
+				console.log('Sorry, that was incorrect');
+			}
+			//add to userGuessedChar to guessedCharacter string
+			guessedCharacter = guessedCharacter + userGuessedChar;		
 		}
+	}else {
+	//display that the character is not A through Z.
+	console.log('The character is not A through Z.')
 	}
-
 })
-
-
 
 
 
@@ -50,4 +50,6 @@ function isLetter(str) {
 	}
 }
 
+
+//create all character buttons to screen
 
